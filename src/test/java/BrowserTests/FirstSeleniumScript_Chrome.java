429e16e6f18b711496ba5fef15a96249124211e3
Bhakti Inamdar <bhaktiinamdar2212@gmail.com>
2024-09-01 19:35:58 -0400

    Dynamically handled radiobuttons and checkboxes

--- src/test/java/BrowserTests/FirstSeleniumScript_Chrome.java
+++ src/test/java/BrowserTests/FirstSeleniumScript_Chrome.java
@@ -48,7 +48,6 @@ public class FirstSeleniumScript_Chrome {
     }
 
     //Locating the elements using browser, navigation and webelement methods
-
     public void checkFunctionalities() throws InterruptedException{
 
        String currentURL=driver.getCurrentUrl();
@@ -72,7 +71,6 @@ public class FirstSeleniumScript_Chrome {
     }
 
     //radio button verification
-
     public void radioButton() throws InterruptedException {
         driver.navigate().to("https://www.automationtestinginsider.com/2019/08/student-registration-form.html");
         System.out.println("User is navigated to automationTestingInsider form page");
@@ -84,65 +82,89 @@ public class FirstSeleniumScript_Chrome {
         driver.navigate().forward();
         Thread.sleep(3000);
         WebElement radioButton = driver.findElement(By.xpath("//input[@value='Female']"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true)",radioButton);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", radioButton);
 
         if (!radioButton.isSelected()) {
             radioButton.click();
-            System.out.println("Is the radiobutton selected:"+radioButton.isSelected());
+            System.out.println("Is the radiobutton selected:" + radioButton.isSelected());
         }
         Thread.sleep(2000);
-    }
 
+        //Dynamically handling radiobutton
+        List<WebElement> radios = driver.findElements(By.xpath(" //input[@name='Gender' and @type='radio']"));
+        for (int i = 0; i < radios.size(); i++) {
+            WebElement rads = radios.get(i);
+            String radiovalues = rads.getAttribute("value");
+            System.out.println(radiovalues);
+            if(radiovalues.contains("Male"))
+            {
+                rads.click();
+            }
+        }
+    }
   /*   WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
        WebElement radiobtn= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='gender-radio-2']")));
        radiobtn.click();
        System.out.println("Radio button is selected:"+radiobtn.isSelected()); //getting false for isSelected()
 */
-    public void checkBox() throws InterruptedException
-    {
-        WebElement checkbox = driver.findElement(By.xpath("//input[@value='Dancing']"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", checkbox);
+        public void checkBox () throws InterruptedException
+        {
+            WebElement checkbox = driver.findElement(By.xpath("//input[@value='Dancing']"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", checkbox);
 
-       if (!checkbox.isSelected())
-       {
-           checkbox.click();
-       }
-        Thread.sleep(2000);
-        System.out.println("Is the checkbox selected:"+checkbox.isSelected());
-        System.out.println("Now going back to Swag labs website ");
+            if (!checkbox.isSelected()) {
+                checkbox.click();
+            }
+            Thread.sleep(2000);
+            System.out.println("Is the checkbox selected:" + checkbox.isSelected());
+            //Select all checkboxes
+            List<WebElement> list = driver.findElements(By.xpath("//input[@name='Hobby' and @type='checkbox']"));
+            for (int i = 0; i < list.size(); i++) {
+                WebElement ele = list.get(i);
+                String hobbies = ele.getAttribute("value");
+                System.out.println(hobbies);
+
+                //Select a particular checkbox from a list of checkboxes
+                if(hobbies.contains("Drawing"))
+                {
+                    ele.click();
+                    Thread.sleep(2000);
+                }
+            }
+        System.out.println("Navigating back to Swag labs website ");
        driver.navigate().back();
        Thread.sleep(2000);
-       }
+        }
 
 
-    public void logout() throws InterruptedException
-    {
-        WebElement logout=driver.findElement(By.xpath("//*[text()='Open Menu']"));
-        logout.click();
-        Thread.sleep(1000);
-        WebElement clickLogout=driver.findElement(By.xpath("//*[contains(text(),'Logout')]"));
-        clickLogout.click();
-        Thread.sleep(1000);
-        System.out.println("logged out of Chrome");
+        public void logout () throws InterruptedException
+        {
+            WebElement logout = driver.findElement(By.xpath("//*[text()='Open Menu']"));
+            logout.click();
+            Thread.sleep(1000);
+            WebElement clickLogout = driver.findElement(By.xpath("//*[contains(text(),'Logout')]"));
+            clickLogout.click();
+            Thread.sleep(1000);
+            System.out.println("logged out of Chrome");
+
+        }
+
+        public void closeBrowser ()
+        {
+            driver.quit();
+        }
+
+        public static void main (String a[]) throws InterruptedException
+        {
+            FirstSeleniumScript_Chrome f = new FirstSeleniumScript_Chrome();
+            f.launchBrowser();
+            f.login();
+            f.title();
+            f.checkFunctionalities();
+            f.radioButton();
+            f.checkBox();
+            f.logout();
+            f.closeBrowser();
+        }
 
     }
-
-    public void closeBrowser()
-    {
-        driver.quit();
-    }
-
-    public static void main(String a[]) throws InterruptedException
-    {
-    FirstSeleniumScript_Chrome f=new FirstSeleniumScript_Chrome();
-    f.launchBrowser();
-    f.login();
-    f.title();
-   f.checkFunctionalities();
-    f.radioButton();
-    f.checkBox();
-    f.logout();
-    f.closeBrowser();
-    }
-}
-
