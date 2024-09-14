33ce201d741448b71287e28548e6f37be1cf8771
Bhakti Inamdar <bhaktiinamdar2212@gmail.com>
2024-09-14 19:10:37 -0400

    Code for alerts, windows, screenshots and scrolls

--- src/test/java/BrowserTests/SecondSeleniumScript_Chrome.java
+++ src/test/java/BrowserTests/SecondSeleniumScript_Chrome.java
@@ -1,5 +1,15 @@
 package BrowserTests;
 
 public class SecondSeleniumScript_Chrome {
-    ////test
+
+    //Scrolling
+    public void scrollTest() throws InterruptedException {
+
+
+    }
+
+    public static void main(String a[]) throws InterruptedException {
+        SecondSeleniumScript_Chrome s=new SecondSeleniumScript_Chrome();
+        s.scrollTest();
+    }
 }
