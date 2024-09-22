79bb65b7e363d2233171920e0f3fddbb3b9f3f36
Bhakti Inamdar <bhaktiinamdar2212@gmail.com>
2024-09-22 18:52:10 -0400

    code for handling dynamic web tables

---
+++ src/test/java/BrowserTests/DynamicWebtables.java
@@ -0,0 +1,80 @@
+package BrowserTests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class DynamicWebtables {
+
+    public static WebDriver driver;
+
+     public void launch() throws InterruptedException
+     {
+         ChromeOptions options=new ChromeOptions();
+         options.addArguments("--incognito");
+         options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation","disable-infobars"));
+         driver=new ChromeDriver(options);
+         driver.manage().deleteAllCookies();
+         driver.manage().window().maximize();
+         driver.manage().timeouts().pageLoadTimeout(5,TimeUnit.SECONDS);
+         driver.get("https://seleniumpractise.blogspot.com/");
+         Thread.sleep(2000);
+     }
+
+     public void Webtables() throws InterruptedException
+     {
+//table headings
+        List <WebElement> table_headings=driver.findElements(By.xpath("//table[@id='customers']//tbody//th"));
+         System.out.println("Table headings are as follows with no of headings as:"+table_headings.size());
+
+         Assert.assertEquals(table_headings.size(),5);
+         boolean status=false;
+        for(WebElement ele:table_headings)
+        {
+           String headings= ele.getText();
+            System.out.println(headings);
+            if(headings.contains("Country"))
+            {
+                status=true;
+            }
+        }
+        Assert.assertTrue(status);
+
+        //capturing table data
+
+         //list rows
+        List<WebElement> rows=driver.findElements(By.xpath("//table[@id='customers']//tbody//tr"));
+        Assert.assertEquals(rows.size(),7,"table row count mismatched" );
+
+         //check if ola is present in the table
+
+         List <WebElement> table_data=driver.findElements(By.xpath("//table[@id='customers']//td"));
+         boolean data_status=false;
+
+        for(WebElement data:table_data)
+        {
+            String text=data.getText();
+            System.out.println(text);
+            if(text.contains("Ola"))
+            {
+                data_status=true;
+                break;
+            }
+        }
+        Assert.assertTrue(data_status,"no record found");
+
+       driver.quit();
+     }
+    public static void main(String[] args) throws InterruptedException
+    {
+        DynamicWebtables d=new DynamicWebtables();
+        d.launch();
+        d.Webtables();
+    }
+}
+
