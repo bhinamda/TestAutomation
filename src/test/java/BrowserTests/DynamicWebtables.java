8e9794cc0a6671ba6b1663ce90de040091702049
Bhakti Inamdar <bhaktiinamdar2212@gmail.com>
2024-09-23 14:10:59 -0400

    enhanced code for dynamic web tables

--- src/test/java/BrowserTests/DynamicWebtables.java
+++ src/test/java/BrowserTests/DynamicWebtables.java
@@ -31,7 +31,6 @@ public class DynamicWebtables {
 //table headings
         List <WebElement> table_headings=driver.findElements(By.xpath("//table[@id='customers']//tbody//th"));
          System.out.println("Table headings are as follows with no of headings as:"+table_headings.size());
-
          Assert.assertEquals(table_headings.size(),5);
          boolean status=false;
         for(WebElement ele:table_headings)
@@ -55,7 +54,6 @@ public class DynamicWebtables {
 
          List <WebElement> table_data=driver.findElements(By.xpath("//table[@id='customers']//td"));
          boolean data_status=false;
-
         for(WebElement data:table_data)
         {
             String text=data.getText();
@@ -68,7 +66,32 @@ public class DynamicWebtables {
         }
         Assert.assertTrue(data_status,"no record found");
 
-       driver.quit();
+        //verify if companies are 6 and java is present or not
+
+         List <WebElement> companyNames=driver.findElements(By.xpath("//table[@id='customers']//tbody//tr//td[2]"));
+
+         boolean companyStatus=false;
+
+         System.out.println("Companies are as follows : ");
+         for(WebElement c:companyNames)
+         {
+             String companiesText=c.getText();
+             System.out.println(companiesText);
+             if(companiesText.contains("Java"))
+             {
+                 companyStatus=true;
+                 break;
+             }
+         }
+         Assert.assertTrue(companyStatus,"Could not find company");
+
+         //click the checkbox/element before selenium
+         //click the link next to ola
+
+         driver.findElement(By.xpath("//td[text()='Selenium']//preceding-sibling::td//input")).click();
+         driver.findElement(By.xpath("//td[text()='Ola']//following-sibling::td[3]")).click();
+
+         driver.quit();
      }
     public static void main(String[] args) throws InterruptedException
     {
